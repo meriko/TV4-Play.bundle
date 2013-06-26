eb19dfe30a8c5b1c678e01446cf09e5ef9041efb
Erik Olsson <meriko@hotmail.com>
2013-06-26 22:29:37 +0200

    Misc: Unicode, URL-Service configuration, Removal of RTMP flag in Info.plist

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -31,13 +31,14 @@ ITEMS_PER_PAGE = 25
 ####################################################################################################
 def Start():
 	Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
+	Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
 	Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, PLUGIN_TITLE, PLUGIN_ICON_DEFAULT, PLUGIN_ARTWORK)
 
 	DirectoryObject.art        = R(ART)
 	DirectoryObject.thumb      = R(THUMB)
 	ObjectContainer.art        = R(ART)
 	ObjectContainer.user_agent = STREAM_USER_AGENT
-	ObjectContainer.view_group = "InfoList"
+	ObjectContainer.view_group = "List"
 	EpisodeObject.art          = R(ART)
 	EpisodeObject.thumb        = R(THUMB)
 	TVShowObject.art           = R(ART)
@@ -53,21 +54,22 @@ def MainMenu():
 	
 	# Add all programs
 	oc.add(
-	  DirectoryObject(key = Callback(TV4Shows, categoryName = "Alla program", 
-	                                           categoryId = " "), 
-		              title = "Alla program", 
-		              thumb = None)
-		  )
+		DirectoryObject(
+			key = Callback(TV4Shows, categoryName = "Alla program", categoryId = " "), 
+			title = "Alla program"
+		)
+	)
 	
 	# Add categories
 	categories = JSON.ObjectFromURL(CATEGORIES_URL)
-  	for category in categories:
+	for category in categories:
 		oc.add(
-		  DirectoryObject(key = Callback(TV4Shows, categoryName = category["name"], 
-		                                           categoryId = category["id"]), 
-		                  title = category["name"], 
-		                  thumb = None)
-		      )
+			DirectoryObject(
+				key = Callback(TV4Shows, categoryName = category["name"], categoryId = category["id"]),
+				title = unicode(category["name"]),
+				thumb = None
+			)
+		)
 
 	# Add preference for video resolution
 	oc.add(PrefsObject(title = u"Inställningar ..."))
@@ -77,29 +79,34 @@ def MainMenu():
 ####################################################################################################
 @route('/video/tv4play/TV4Shows')
 def TV4Shows(categoryName, categoryId):
-	oc         = ObjectContainer(title2 = categoryName)
+	oc         = ObjectContainer(title2 = unicode(categoryName))
 	categoryId = urllib2.quote(categoryId)
 	programs   = JSON.ObjectFromURL(PROGRAMS_URL % categoryId)
 
 	for program in programs:
 		oc.add(
-		  DirectoryObject(key = Callback(TV4ShowChoice, showName = program["name"], 
-		                                                showId = program["id"], 
-		                                                art = getImgUrl(program["largeimage_highres"]),
-		                                                thumb = getImgUrl(program["image"]),
-		                                                summary = program["text"]), 
-		                  title = program["name"],
-		                  summary = program["text"], 
-		                  thumb = getImgUrl(program["image"]),
-		                  art = getImgUrl(program["largeimage_highres"]))
-		      )
+			DirectoryObject( 
+				key = 
+					Callback(TV4ShowChoice, 
+						showName = program["name"], 
+						showId = program["id"], 
+						art = getImgUrl(program["largeimage_highres"]),
+						thumb = getImgUrl(program["image"]),
+						summary = program["text"]
+					), 
+				title = unicode(program["name"]),
+				summary = unicode(program["text"]), 
+				thumb = getImgUrl(program["image"]),
+				art = getImgUrl(program["largeimage_highres"])
+			)
+		)
 
 	return oc
 
 ####################################################################################################
 @route('/video/tv4play/TV4Shows/TV4ShowChoice')
 def TV4ShowChoice(showName, showId, art, thumb, summary):
-	oc = ObjectContainer(title2 = showName)
+	oc = ObjectContainer(title2 = unicode(showName))
 		
 	showId   = urllib2.quote(showId)
 	episodes = JSON.ObjectFromURL(EPISODES_URL % (0, 0, showId))
@@ -107,69 +114,81 @@ def TV4ShowChoice(showName, showId, art, thumb, summary):
 		
 	if episodes['total_hits'] > 0 and clips['total_hits'] > 0: 
 		oc.add(
-		  	DirectoryObject(key = Callback(TV4Videos, showName = showName, 
-		    	                                      showId = showId, 
-		                                              art = art,
-		                                              episodeReq = True), 
-		                    title = "Hela program",
-		                    thumb = thumb,
-		                    summary = summary,
-		                    art = art)
-		      )
+			DirectoryObject(key = 
+								Callback(TV4Videos, 
+									showName = showName, 
+									showId = showId, 
+									art = art,
+									episodeReq = True
+								), 
+							title = "Hela program",
+							thumb = thumb,
+							summary = unicode(summary),
+							art = art
+			)
+		)
 		      	  	
 		oc.add(
-			DirectoryObject(key = Callback(TV4Videos, showName = showName, 
-		                                              showId = showId, 
-		                                              art = art,
-		                                              episodeReq = False), 
-		                    title = "Klipp",
-		                    thumb = thumb,
-		                    summary = summary,
-		                    art = art)
-		      )
+			DirectoryObject(
+							key = 
+								Callback(TV4Videos, 
+									showName = showName, 
+									showId = showId, 
+									art = art,
+									episodeReq = False
+								), 
+							title = "Klipp",
+							thumb = thumb,
+							summary = unicode(summary),
+							art = art
+			)
+		)
 		      
 	elif episodes['total_hits'] > 0 or clips['total_hits'] > 0:
-		return TV4Videos(showName = showName, 
-		                 showId = showId, 
-		                 art = art,
-		                 episodeReq = episodes['total_hits'] > 0)
+		return TV4Videos(
+				showName = showName, 
+				showId = showId, 
+				art = art,
+				episodeReq = episodes['total_hits'] > 0
+		)
 			  
 	return oc
 
 ####################################################################################################
 @route('/video/tv4play/TV4Shows/TV4Videos', episodeReq = bool, offset = int)
 def TV4Videos(showName, showId, art, episodeReq, offset = 0):
-	oc = ObjectContainer(title2 = showName)
+	oc = ObjectContainer(title2 = unicode(showName))
+	oc.view_group = "InfoList"
 
 	if episodeReq: 
 		videos = JSON.ObjectFromURL(EPISODES_URL % (offset, ITEMS_PER_PAGE, showId))
 	else:
 		videos = JSON.ObjectFromURL(CLIPS_URL % (offset, ITEMS_PER_PAGE, showId))
 
-  	# Add videos from JSON info
+	# Add videos from JSON info
 	for video in videos['results']:
-  		url = BASE_URL + "/program/" + "%s?video_id=%s" % (showId, str(video['vmanprogid']))
+		url = BASE_URL + "/program/" + "%s?video_id=%s" % (showId, str(video['vmanprogid']))
     			    	
-  		try:
+		try:
 			publishdate = str(video['publishdate'])
 			year        = int(publishdate[0:4])
 			month       = int(publishdate[4:6])
 			day         = int(publishdate[6:8])
 			airdate     = datetime.date(year, month, day)
-  		except:
+		except:
 			airdate = None
   		  
-  		if video['lead'] != None:
-  			description = video['lead']
-  		else:
-  			description = ""  
+		if video['lead'] != None:
+			description = video['lead']
+		else:
+			description = ""  
   		  
 		try:
 			episode = int(re.search('Del ([0-9]+)', video['name']).group(0))
 		except:
 			episode = None 		  
   		  
-  		if video['availability']['human'] != None:
+		if video['availability']['human'] != None:
 			availabilty = video['availability']['human']
 		else:
 			availabilty = ""		 	 		  
@@ -185,40 +204,48 @@ def TV4Videos(showName, showId, art, episodeReq, offset = 0):
 
 		if episodeReq:
 			oc.add(
-		  		EpisodeObject(
-            		url = url,
-            		title = video['name'],
-            		index = episode,
-            		summary = description + " " + availabilty,
-            		show = showName,
-            		thumb = video['originalimage'],
-            		directors = video['keywords'],
-            		art = art,
-            		originally_available_at = airdate))		
+				EpisodeObject(
+					url = url,
+					title = unicode(video['name']),
+					index = episode,
+					summary = unicode(description + " " + availabilty),
+            		show = unicode(showName),
+					thumb = video['originalimage'],
+					art = art,
+					originally_available_at = airdate
+				)
+			)		
 		else:
 			oc.add(
-      			VideoClipObject(
-        			url = url,
-        			title = video['name'],
-        			summary = description + " " + availabilty,
-        			thumb = video['originalimage'],
-        			art = art,
-        			originally_available_at = airdate))	    
+				VideoClipObject(
+					url = url,
+					title = unicode(video['name']),
+					summary = unicode(description + " " + availabilty),
+					thumb = video['originalimage'],
+					art = art,
+					originally_available_at = airdate
+				)
+			)	    
 
 	if offset + ITEMS_PER_PAGE < videos['total_hits']:
 		nextPage = (offset / ITEMS_PER_PAGE) + 2
 		lastPage = (videos['total_hits'] / ITEMS_PER_PAGE) + 1
 		oc.add(
-		  	NextPageObject(key = Callback(TV4Videos, showName = showName, 
-		    	                                     showId = showId, 
-		       		                                 art = art,
-		      		                                 episodeReq = episodeReq,
-	              		                             offset = offset + ITEMS_PER_PAGE), 
-	                   	   title = "Fler ...",
-	                  	   summary = "Vidare till sida " + str(nextPage) + " av " + str(lastPage),
-	                  	   thumb = R(PLUGIN_ICON_MORE),
-	                  	   art = art)
-      		)	
+			NextPageObject(
+				key = 
+					Callback(TV4Videos,
+						showName = showName, 
+						showId = showId, 
+						art = art,
+						episodeReq = episodeReq,
+						offset = offset + ITEMS_PER_PAGE
+					), 
+				title = "Fler ...",
+				summary = "Vidare till sida " + str(nextPage) + " av " + str(lastPage),
+				thumb = R(PLUGIN_ICON_MORE),
+				art = art
+			)
+		)	
 
 	return oc
 
